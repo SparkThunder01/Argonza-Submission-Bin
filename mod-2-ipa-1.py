7fa4fb6c1aeec5a6a7a1b0ef6d2c86f77559ac3f
SparkThunder01 <75445658+SparkThunder01@users.noreply.github.com>
2022-06-20 22:47:54 +0800

    Add files via upload

--- mod-2-ipa-1.py
+++ mod-2-ipa-1.py
@@ -98,7 +98,7 @@ def interest(principal, rate, periods):
     '''
     # Replace `pass` with your code. 
     # Stay within the function. Only use the parameters as input. The function should return your answer.
-    return principal*(float(rate.replace("%", ""))/100)*periods+principal
+    return principal*rate*periods+principal
 
 def body_mass_index(weight, height):
     '''Body Mass Index.
