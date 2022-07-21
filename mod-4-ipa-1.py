ed8cb000083bf01cc467aca1f7502d9a9ef52b69
SparkThunder01 <75445658+SparkThunder01@users.noreply.github.com>
2022-07-21 18:35:01 +0800

    Add files via upload

--- mod-4-ipa-1.py
+++ mod-4-ipa-1.py
@@ -41,42 +41,6 @@ def relationship_status(from_member, to_member, social_graph):
     '''
     # Replace `pass` with your code. 
     # Stay within the function. Only use the parameters as input. The function should return your answer.
-    def relationship_status(from_member, to_member, social_graph):
-    '''Relationship Status.
-    20 points.
-
-    Let us pretend that you are building a new app.
-    Your app supports social media functionality, which means that users can have
-    relationships with other users.
-
-    There are two guidelines for describing relationships on this social media app:
-    1. Any user can follow any other user.
-    2. If two users follow each other, they are considered friends.
-
-    This function describes the relationship that two users have with each other.
-
-    Please see "assignment-4-sample-data.py" for sample data. The social graph
-    will adhere to the same pattern.
-
-    Parameters
-    ----------
-    from_member: str
-        the subject member
-    to_member: str
-        the object member
-    social_graph: dict
-        the relationship data    
-
-    Returns
-    -------
-    str
-        "follower" if fromMember follows toMember,
-        "followed by" if fromMember is followed by toMember,
-        "friends" if fromMember and toMember follow each other,
-        "no relationship" if neither fromMember nor toMember follow each other.
-    '''
-    # Replace `pass` with your code. 
-    # Stay within the function. Only use the parameters as input. The function should return your answer.
     a = social_graph[from_member]
     b = social_graph[to_member] 
     
@@ -85,7 +49,7 @@ def relationship_status(from_member, to_member, social_graph):
     elif from_member in b['following']:
         return "followed by"
     elif to_member in a['following']:
-        return "following"
+        return "follower"
     else:
         return "no relationship"    
     
@@ -120,7 +84,7 @@ def tic_tac_toe(board):
     horizontal = [x for x in board]
     vertical = [x for x in zip(*board)]
     diagonal_up_down = [board[i][i] for i, v in enumerate(board)]
-    diagonal_down_up = [board[2-i][i] for i, v in enumerate(board)]
+    diagonal_down_up = [board[len(board)-i][i] for i, v in enumerate(board)]
     
     
     
